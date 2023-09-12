505f983d12c23acaba964f2671c4f8e5bac8ca5c
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2023-09-12 12:06:21 -0300

    modifica length das colunas da tabela de request

--- db-updates.php
+++ db-updates.php
@@ -60,16 +60,18 @@ return [
         __exec("DELETE FROM db_update WHERE name = 'create view blame'");
     },
 
-    'alter table blame_request columns length' => function(){
+    'alter table blame_request columns length again' => function(){
         __try("DROP VIEW blame");
         
-        __exec("ALTER TABLE blame_request ALTER COLUMN user_os TYPE varchar(128)");
-        __exec("ALTER TABLE blame_request ALTER COLUMN user_device TYPE varchar(128)");
-        __exec("ALTER TABLE blame_request ALTER COLUMN user_browser_name TYPE varchar(128)");
-        __exec("ALTER TABLE blame_request ALTER COLUMN user_browser_version TYPE varchar(128)");
+        __exec("ALTER TABLE blame_request ALTER COLUMN user_os TYPE varchar(512)");
+        __exec("ALTER TABLE blame_request ALTER COLUMN user_device TYPE varchar(512)");
+        __exec("ALTER TABLE blame_request ALTER COLUMN user_browser_name TYPE varchar(512)");
+        __exec("ALTER TABLE blame_request ALTER COLUMN user_browser_version TYPE varchar(512)");
+        __exec("ALTER TABLE blame_request ALTER COLUMN user_agent TYPE varchar(1024)");
         
         __exec("DELETE FROM db_update WHERE name = 'create view blame'");
     },
+    
 
     'create view blame' => function () {
         __exec("CREATE VIEW blame AS (
