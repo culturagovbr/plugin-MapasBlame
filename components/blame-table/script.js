78a03f217d41cc15134f3be0b5c7998676343093
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2024-09-04 16:02:21 -0300

    Ajustes nas traduções da tabela de logs (Ref.: #16)

--- components/blame-table/script.js
+++ components/blame-table/script.js
@@ -49,7 +49,7 @@ app.component('blame-table', {
 
     data() {
         let query = {
-            '@select': 'id,sessionId,requestId,requestTimestamp,action,logTimestamp,userBrowserName,userBrowserVersion,ip,userId,userOS,userDevice',
+            '@select': 'id,sessionId,requestId,requestTimestamp,action,logTimestamp,userBrowserName,userBrowserVersion,ip,userId,userOS,userDevice,user.{email,profile.{name,avatar}}',
         };
 
         if (this.userId) {
@@ -68,65 +68,67 @@ app.component('blame-table', {
             selectedActions: [],
 
             actionOptions: {
+                // REQUISIÇÕES GERAIS
                 'GET': __('REQUISIÇÕES - GET (acessos)', 'blame-table'),
                 'POST': __('REQUISIÇÕES - POST (Criações e outras ações)', 'blame-table'),
                 'PATCH': __('REQUISIÇÕES - PATCH (Atualizações parciais)', 'blame-table'),
                 'PUT': __('REQUISIÇÕES - PUT (Atualizações completas)', 'blame-table'),
                 'DELETE': __('REQUISIÇÕES - DELETE (Exclusões)', 'blame-table'),
 
-                'POST%registration.sendEvaluation%': __('Envio de Avaliações'),
+                // AVALIAÇÕES
+                'POST%registration.sendEvaluation%': __('Envio de Avaliações', 'blame-table'),
 
                 // INSCRIÇÕES
-                'POST%(registration.index)': __('INSCRIÇÕES - criação de inscrição'),
-                'PATCH%(registration.single)': __('INSCRIÇÕES - modificação de inscrição'),
-                'POST%(registration.send)': __('INSCRIÇÕES - envio de inscrição'),
-                'POST%(registration.validateEntity)': __('INSCRIÇÕES - validação de inscrição'),
-                'DELETE%(registration.single)': __('INSCRIÇÕES - exclusão de inscrição'),
+                'POST%(registration.index)': __('INSCRIÇÕES - criação de inscrição', 'blame-table'),
+                'PATCH%(registration.single)': __('INSCRIÇÕES - modificação de inscrição', 'blame-table'),
+                'POST%(registration.send)': __('INSCRIÇÕES - envio de inscrição', 'blame-table'),
+                'POST%(registration.validateEntity)': __('INSCRIÇÕES - validação de inscrição', 'blame-table'),
+                'DELETE%(registration.single)': __('INSCRIÇÕES - exclusão de inscrição', 'blame-table'),
 
                 // AGENTES
-                'GET%(agent.single)': __('AGENTES - acesso à página'),
-                'POST%(agent.index)': __('AGENTES - criação de agente'),
-                'PATCH%(agent.single)': __('AGENTES - modificação de agente'),
-                'DELETE%(agent.single)': __('AGENTES - exclusão de agente'),
+                'GET%(agent.single)': __('AGENTES - acesso à página', 'blame-table'),
+                'POST%(agent.index)': __('AGENTES - criação de agente', 'blame-table'),
+                'PATCH%(agent.single)': __('AGENTES - modificação de agente', 'blame-table'),
+                'DELETE%(agent.single)': __('AGENTES - exclusão de agente', 'blame-table'),
 
                 // ESPAÇOS
-                'GET%(space.single)': __('ESPAÇOS - acesso à página'),
-                'POST%(space.index)': __('ESPAÇOS - criação de espaço'),
-                'PATCH%(space.single)': __('ESPAÇOS - modificação de espaço'),
-                'DELETE%(space.single)': __('ESPAÇOS - exclusão de espaço'),
+                'GET%(space.single)': __('ESPAÇOS - acesso à página', 'blame-table'),
+                'POST%(space.index)': __('ESPAÇOS - criação de espaço', 'blame-table'),
+                'PATCH%(space.single)': __('ESPAÇOS - modificação de espaço', 'blame-table'),
+                'DELETE%(space.single)': __('ESPAÇOS - exclusão de espaço', 'blame-table'),
 
                 // PROJETOS
-                'GET%(project.single)': __('PROJETOS - acesso à página'),
-                'POST%(project.index)': __('PROJETOS - criação de projeto'),
-                'PATCH%(project.single)': __('PROJETOS - modificação de projeto'),
-                'DELETE%(project.single)': __('PROJETOS - exclusão de projeto'),
+                'GET%(project.single)': __('PROJETOS - acesso à página', 'blame-table'),
+                'POST%(project.index)': __('PROJETOS - criação de projeto', 'blame-table'),
+                'PATCH%(project.single)': __('PROJETOS - modificação de projeto', 'blame-table'),
+                'DELETE%(project.single)': __('PROJETOS - exclusão de projeto', 'blame-table'),
                 
                 // EVENTOS
-                'GET%(event.single)': __('EVENTOS - acesso à página'),
-                'POST%(event.index)': __('EVENTOS - criação de evento'),
-                'PATCH%(event.single)': __('EVENTOS - modificação de evento'),
-                'DELETE%(event.single)': __('EVENTOS - exclusão de evento'),
-                'POST%(eventoccurrence.index)': __('EVENTOS - criação de ocorrência'),
-                'DELETE%(eventoccurrence.single)': __('EVENTOS - exclusão de ocorrência'),
+                'GET%(event.single)': __('EVENTOS - acesso à página', 'blame-table'),
+                'POST%(event.index)': __('EVENTOS - criação de evento', 'blame-table'),
+                'PATCH%(event.single)': __('EVENTOS - modificação de evento', 'blame-table'),
+                'DELETE%(event.single)': __('EVENTOS - exclusão de evento', 'blame-table'),
+                'POST%(eventoccurrence.index)': __('EVENTOS - criação de ocorrência', 'blame-table'),
+                'DELETE%(eventoccurrence.single)': __('EVENTOS - exclusão de ocorrência', 'blame-table'),
 
                 // OPORTUNIDADES
-                'GET%(opportunity.single)': __('OPORTUNIDADES - acesso à página'),
-                'GET%(opportunity.edit)': __('OPORTUNIDADES - acesso à página de gestão'),
-                'POST%(opportunity.index)': __('OPORTUNIDADES - criação de oportunidade'),
-                'PATCH%(opportunity.single)': __('OPORTUNIDADES - modificação de oportunidade'),
-                'DELETE%(opportunity.single)': __('OPORTUNIDADES - exclusão de oportunidade'),
+                'GET%(opportunity.single)': __('OPORTUNIDADES - acesso à página', 'blame-table'),
+                'GET%(opportunity.edit)': __('OPORTUNIDADES - acesso à página de gestão', 'blame-table'),
+                'POST%(opportunity.index)': __('OPORTUNIDADES - criação de oportunidade', 'blame-table'),
+                'PATCH%(opportunity.single)': __('OPORTUNIDADES - modificação de oportunidade', 'blame-table'),
+                'DELETE%(opportunity.single)': __('OPORTUNIDADES - exclusão de oportunidade', 'blame-table'),
 
                 // OPORTUNIDADES - fases
-                'POST%(evaluationmethodconfiguration.index)': __('OPORTUNIDADES - fase de avaliação - criação'),
-                'POST%(evaluationmethodconfiguration.index)': __('OPORTUNIDADES - fase de avaliação - exclusão'),
+                'POST%(evaluationmethodconfiguration.index)': __('OPORTUNIDADES - fase de avaliação - criação', 'blame-table'),
+                'POST%(evaluationmethodconfiguration.index)': __('OPORTUNIDADES - fase de avaliação - exclusão', 'blame-table'),
 
                 // OPORTUNIDADES - campos
-                'POST%(registrationfieldconfiguration.index)': __('OPORTUNIDADES - configuração do formulário - criação de campo'),
-                'POST%(registrationfieldconfiguration.single)': __('OPORTUNIDADES - configuração do formulário - modificação de campo'),
-                'GET%(registrationfieldconfiguration.delete)': __('OPORTUNIDADES - configuração do formulário - exclusão de campo'),
-                'POST%(registrationfileconfiguration.index)': __('OPORTUNIDADES - configuração do formulário - criação de anexo'),
-                'POST%(registrationfileconfiguration.single)': __('OPORTUNIDADES - configuração do formulário - modificação de anexo'),
-                'POST%(registrationfileconfiguration.delete)': __('OPORTUNIDADES - configuração do formulário - exclusão de anexo'),
+                'POST%(registrationfieldconfiguration.index)': __('OPORTUNIDADES - configuração do formulário - criação de campo', 'blame-table'),
+                'POST%(registrationfieldconfiguration.single)': __('OPORTUNIDADES - configuração do formulário - modificação de campo', 'blame-table'),
+                'GET%(registrationfieldconfiguration.delete)': __('OPORTUNIDADES - configuração do formulário - exclusão de campo', 'blame-table'),
+                'POST%(registrationfileconfiguration.index)': __('OPORTUNIDADES - configuração do formulário - criação de anexo', 'blame-table'),
+                'POST%(registrationfileconfiguration.single)': __('OPORTUNIDADES - configuração do formulário - modificação de anexo', 'blame-table'),
+                'POST%(registrationfileconfiguration.delete)': __('OPORTUNIDADES - configuração do formulário - exclusão de anexo', 'blame-table'),
                 
             },
         }
--- components/blame-table/texts.php
+++ components/blame-table/texts.php
@@ -14,31 +14,65 @@ return [
     'Sistema Operacional' => i::__('Sistema Operacional'),
     'Dispositivo' => i::__('Dispositivo'),
 
-    'Acessos' => i::__('Acessos'),
-    'Atualizações parciais' => i::__('Atualizações parciais'),
-    
-    'Envio de Inscrições' => i::__('Envio de Inscrições'),
+    // REQUISIÇÕES
+    'REQUISIÇÕES - GET (acessos)' => i::__('REQUISIÇÕES - GET (acessos)'),
+    'REQUISIÇÕES - POST (Criações e outras ações)' => i::__('REQUISIÇÕES - POST (Criações e outras ações)'),
+    'REQUISIÇÕES - PATCH (Atualizações parciais)' => i::__('REQUISIÇÕES - PATCH (Atualizações parciais)'),
+    'REQUISIÇÕES - PUT (Atualizações completas)' => i::__('REQUISIÇÕES - PUT (Atualizações completas)'),
+    'REQUISIÇÕES - DELETE (Exclusões)' => i::__('REQUISIÇÕES - DELETE (Exclusões)'),
+
+    // AVALIAÇÕES
     'Envio de Avaliações' => i::__('Envio de Avaliações'),
+
+    // INSCRIÇÕES
+    'INSCRIÇÕES - criação de inscrição' => i::__('INSCRIÇÕES - criação de inscrição'),
+    'INSCRIÇÕES - modificação de inscrição' => i::__('INSCRIÇÕES - modificação de inscrição'),
+    'INSCRIÇÕES - envio de inscrição' => i::__('INSCRIÇÕES - envio de inscrição'),
+    'INSCRIÇÕES - validação de inscrição' => i::__('INSCRIÇÕES - validação de inscrição'),
+    'INSCRIÇÕES - exclusão de inscrição' => i::__('INSCRIÇÕES - exclusão de inscrição'),
+
+    // AGENTES
+    'AGENTES - acesso à página' => i::__('AGENTES - acesso à página'),
+    'AGENTES - criação de agente' => i::__('AGENTES - criação de agente'),
+    'AGENTES - modificação de agente' => i::__('AGENTES - modificação de agente'),
+    'AGENTES - exclusão de agente' => i::__('AGENTES - exclusão de agente'),
+
+    // ESPAÇOS
+    'ESPAÇOS - acesso à página' => i::__('ESPAÇOS - acesso à página'),
+    'ESPAÇOS - criação de espaço' => i::__('ESPAÇOS - criação de espaço'),
+    'ESPAÇOS - modificação de espaço' => i::__('ESPAÇOS - modificação de espaço'),
+    'ESPAÇOS - exclusão de espaço' => i::__('ESPAÇOS - exclusão de espaço'),
+
+    // PROJETOS
+    'PROJETOS - acesso à página' => i::__('PROJETOS - acesso à página'),
+    'PROJETOS - criação de projeto' => i::__('PROJETOS - criação de projeto'),
+    'PROJETOS - modificação de projeto' => i::__('PROJETOS - modificação de projeto'),
+    'PROJETOS - exclusão de projeto' => i::__('PROJETOS - exclusão de projeto'),
     
-    
-    'Criações' => i::__('Criações'),
-    'Criação de Agentes' => I::__('Criação de Agentes'),
-    'Criação de Oportunidades' => i::__('Criação de Oportunidades'),
-    'Criação de Eventos' => i::__('Criação de Eventos'),
-    'Criação de Espaços' => i::__('Criação de Espaços'),
-    'Criação de Projetos' => i::__('Criação de Projetos'),
-    
-    'Atualizações' => i::__('Atualizações'),
-    'Atualização de Agentes' => i::__('Atualização de Agentes'),
-    'Atualização de Oportunidades' => i::__('Atualização de Oportunidades'),
-    'Atualização de Eventos' => i::__('Atualização de Eventos'),
-    'Atualização de Espaços' => i::__('Atualização de Espaços'),
-    'Atualização de Projetos' => i::__('Atualização de Projetos'),
-    
-    'Exclusões' => i::__('Exclusões'),
-    'Exclusão de Agentes' => i::__('Exclusão de Agentes'),
-    'Exclusão de Oportunidades' => i::__('Exclusão de Oportunidades'),
-    'Exclusão de Eventos' => i::__('Exclusão de Eventos'),
-    'Exclusão de Espaços' => i::__('Exclusão de Espaços'),
-    'Exclusão de Projetos' => i::__('Exclusão de Projetos'),
+    // EVENTOS
+    'EVENTOS - acesso à página' => i::__('EVENTOS - acesso à página'),
+    'EVENTOS - criação de evento' => i::__('EVENTOS - criação de evento'),
+    'EVENTOS - modificação de evento' => i::__('EVENTOS - modificação de evento'),
+    'EVENTOS - exclusão de evento' => i::__('EVENTOS - exclusão de evento'),
+    'EVENTOS - criação de ocorrência' => i::__('EVENTOS - criação de ocorrência'),
+    'EVENTOS - exclusão de ocorrência' => i::__('EVENTOS - exclusão de ocorrência'),
+
+    // OPORTUNIDADES
+    'OPORTUNIDADES - acesso à página' => i::__('OPORTUNIDADES - acesso à página'),
+    'OPORTUNIDADES - acesso à página de gestão' => i::__('OPORTUNIDADES - acesso à página de gestão'),
+    'OPORTUNIDADES - criação de oportunidade' => i::__('OPORTUNIDADES - criação de oportunidade'),
+    'OPORTUNIDADES - modificação de oportunidade' => i::__('OPORTUNIDADES - modificação de oportunidade'),
+    'OPORTUNIDADES - exclusão de oportunidade' => i::__('OPORTUNIDADES - exclusão de oportunidade'),
+
+    // OPORTUNIDADES - fases
+    'OPORTUNIDADES - fase de avaliação - criação' => i::__('OPORTUNIDADES - fase de avaliação - criação'),
+    'OPORTUNIDADES - fase de avaliação - exclusão' => i::__('OPORTUNIDADES - fase de avaliação - exclusão'),
+
+    // OPORTUNIDADES - campos
+    'OPORTUNIDADES - configuração do formulário - criação de campo' => i::__('OPORTUNIDADES - configuração do formulário - criação de campo'),
+    'OPORTUNIDADES - configuração do formulário - modificação de campo' => i::__('OPORTUNIDADES - configuração do formulário - modificação de campo'),
+    'OPORTUNIDADES - configuração do formulário - exclusão de campo' => i::__('OPORTUNIDADES - configuração do formulário - exclusão de campo'),
+    'OPORTUNIDADES - configuração do formulário - criação de anexo' => i::__('OPORTUNIDADES - configuração do formulário - criação de anexo'),
+    'OPORTUNIDADES - configuração do formulário - modificação de anexo' => i::__('OPORTUNIDADES - configuração do formulário - modificação de anexo'),
+    'OPORTUNIDADES - configuração do formulário - exclusão de anexo' => i::__('OPORTUNIDADES - configuração do formulário - exclusão de anexo'),
 ];
