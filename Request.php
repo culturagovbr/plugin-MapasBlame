bc0eed3310633feef52fe80f41d820039b10eb77
Lucas Oliveira <lucas.fullstack.dev@gmail.com>
2026-05-01 12:29:52 -0300

    feat: obtendo IP a partir da propriedadde request da PRS-7

--- Request.php
+++ Request.php
@@ -33,7 +33,8 @@ class Request {
         $this->id = $app->getToken(13);
         $this->metadata = (object) $metadata;
         
-        $this->ip = $_SERVER['HTTP_CF_CONNECTING_IP'] ?? $_SERVER['HTTP_X_REAL_IP'] ?? $_SERVER['HTTP_X_FORWARDED_FOR'] ?? $_SERVER['REMOTE_ADDR'] ?? '';
+        // Obtendo o IP a partir da propriedade request da PSR-7
+        $this->ip = $app->request->getIp();
         $this->userAgent = $_SERVER['HTTP_USER_AGENT'] ?? '';
         $this->sessionId = session_id();
         $this->userId = $app->user->id;
